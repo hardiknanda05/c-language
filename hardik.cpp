651f3ac4bb33bbe509322426f56be09734b94a30
hardiknanda <140815083+hardiknanda05@users.noreply.github.com>
2023-08-02 08:50:42 +0530

    Add files via upload

---
+++ hardik.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   printf("NAME=\"nanda hardik\" \n");
+   printf("SEM=\"first B.tech CSE\"\n");
+   printf("roll no.=146\n");
+   printf("address=jamnagar");
+   return 0;
+}   
+
